7e0d916a178e8dbc047f8b99251292d4ce1c3604
Andre <aamelendez@scl.edu.gt>
2025-08-28 20:22:44 -0600

    Version nueva06

--- routers/get/obtenerTablas.js
+++ routers/get/obtenerTablas.js
@@ -2,13 +2,16 @@ import express from 'express';
 import { pool } from '../../config/dataBase.js';
 const router = express.Router();
 
-router.get('/tablas', async (req, res) => {  // solo '/tablas'
+router.get('/tablas', async (req, res) => {
   try {
     const [tablas] = await pool.query('SHOW TABLES;');
     res.json(tablas);
   } catch (err) {
     console.error('Error en /tablas:', err.message);
-    res.status(500).json({ error: err.message });
+    res.status(500).json({
+      error: 'Error interno del servidor',
+      detalle: err.message
+    });
   }
 });
 
